7374cc13825dd789cfc4f77cc865d9a29335ece2
Chitte.Amol <Chitte.Amol@SYNPUNLT2ZCS0C2.SYNCSS.LOCAL>
2020-03-03 19:37:14 +0530

    removed project1-master

--- Project1/src/main/java/com/project1/webelements/Page1Elements.java
+++ Project1/src/main/java/com/project1/webelements/Page1Elements.java
@@ -4,27 +4,23 @@ import org.openqa.selenium.By;
 
 import com.wma.framework.common.ByFactory;
 import com.wma.framework.common.ByType;
-import com.wma.framework.common.Frame;
 import com.wma.framework.common.PageElement;
 
 import static com.wma.framework.common.ByType.*;
-import static com.wma.framework.common.Frame.*;
 
 public enum Page1Elements implements PageElement {
-	LEARN_HTML_LINK(LINK_TEXT, "LEARN HTML", DEFAULT),
-	NEXT(XPATH, "//a[contains(text(), 'Next')]", DEFAULT),
-	PREVIOUS(XPATH, "//a[contains(text(), 'Previous')]", DEFAULT),
-	HOME(XPATH, "//a[contains(text(), 'Next')]", DEFAULT),
+	LEARN_HTML_LINK(LINK_TEXT, "LEARN HTML"),
+	NEXT(XPATH, "//a[contains(text(), 'Next')]"),
+	PREVIOUS(XPATH, "//a[contains(text(), 'Previous')]"),
+	HOME(XPATH, "//a[contains(text(), 'Next')]"),
 	;
 	
 	private final ByType BY_TYPE;
-	private final Frame FRAME_NAME;
 	private final String EXPRESSION;
 	
-	private Page1Elements(ByType byType, String expression, Frame frame) {
+	private Page1Elements(ByType byType, String expression) {
 		this.BY_TYPE = byType;
 		this.EXPRESSION = expression;
-		this.FRAME_NAME = frame;
 	}
 	
 	public By getBy(String... arg0) {
@@ -33,10 +29,6 @@ public enum Page1Elements implements PageElement {
 
 	public String getExpression() {
 		return EXPRESSION;
-	}
-
-	public Frame getFrame() {
-		return FRAME_NAME;
 	}
 
 	public ByType getType() {
